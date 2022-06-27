ec073e2da7d157849f93ec3d6e908daf1c96a093
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2022-06-27 14:06:36 +1000

    load json from nasa api

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydoc import cli
 import discord
 from discord.ext import commands
 import requests
@@ -19,5 +20,26 @@ DISCORD_API = os.getenv("DISCORD_TOKEN")
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=""))
+
+# ping command
+
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+NASA_API = os.getenv("NASA_KEY")
+
+
+@client.command()
+async def apod(ctx):
+
+    response = requests.get(
+        "https://api.nasa.gov/planetary/apod?api_key=" + NASA_API)
+    json_data = json.loads(response.text)
+    channel = ctx.message.channel
+
+    await ctx.send(json_data)
+
 
 client.run(DISCORD_API)

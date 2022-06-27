f14120239210e3cbb8bfe4e48c99a8e3823e4616
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2022-06-27 13:36:25 +1000

    add token, bot initialised

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ import os
 
 load_dotenv()
 client = commands.Bot(command_prefix='!util ')
+client.remove_command("help")
+DISCORD_API = os.getenv("DISCORD_TOKEN")
+
+# initialise
+
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=""))

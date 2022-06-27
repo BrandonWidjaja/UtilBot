06c3349e68d4d75fe17016783c692a92dc148b4a
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2022-06-27 14:36:08 +1000

    finished apod command, and added help command

--- main.py
+++ main.py
@@ -39,7 +39,41 @@ async def apod(ctx):
     json_data = json.loads(response.text)
     channel = ctx.message.channel
 
-    await ctx.send(json_data)
+    async with channel.typing():
 
+        embed = discord.Embed(title=f"Astronomy Picture of the Day: \n{json_data['title']}",
+                              color=ctx.guild.me.top_role.color,
+                              timestamp=ctx.message.created_at,)
+
+        embed.add_field(
+            name="Date Taken", value=f"**{json_data['date']}**", inline=False)
+        embed.set_image(url=json_data['hdurl'])
+        embed.add_field(
+            name="HD Image:", value=f"**{json_data['hdurl']}**", inline=False)
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}")
+
+    await channel.send(embed=embed)
+
+
+@client.command(pass_context=True)
+async def help(ctx):
+
+    channel = ctx.message.channel
+    seperator = '-------------------------------------------\n'
+
+    embed = discord.Embed(title=f"Commands",
+                          color=discord.Color.purple(),
+                          timestamp=ctx.message.created_at,)
+
+    embed.add_field(name=f'{seperator}ping',
+                    value='Returns ping of the bot\n**Usage**: !util ping', inline=False)
+    embed.add_field(name=f'{seperator}apod',
+                    value='Returns NASA\'s Astrology Picture of the Day\n**Usage**: !util apod', inline=False)
+
+    embed.set_footer(
+        text=f"Requested by {ctx.author.name}")
+
+    await channel.send(embed=embed)
 
 client.run(DISCORD_API)
